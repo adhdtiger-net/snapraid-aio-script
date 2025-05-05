c5489811ae1ed2911ba2c283ea2777186a0be378
adhdtiger <adhdtiger.net@gmail.com>
2025-05-05 18:33:05 -0400

    some stuff working haha

--- snapraid-aio-script.sh
+++ snapraid-aio-script.sh
@@ -876,10 +876,10 @@ function notify_snapraid_info() {
       -d parse_mode="markdown"
   fi
   if [ "$DISCORD" -eq 1 ]; then
-    INFO_MESSAGE_ESCAPED=$(echo "$INFO_MESSAGE_DISCORD" | jq -Rs | cut -c 2- | rev | cut -c 2- | rev)
+    INFO_MESSAGE_ESCAPED="$(echo "$INFO_MESSAGE_DISCORD" | jq -Rs | cut -c 2- | rev | cut -c 2- | rev)"
     curl -fsS -m 5 --retry 3 -o /dev/null -X POST \
       -H 'Content-Type: application/json' \
-      -d '{"content": "\`\`\`"\n${INFO_MESSAGE_ESCAPED}\n"\`\`\`"}' \
+      -d '{"content": '"$()$("\n""$INFO_MESSAGE_ESCAPED""\n")$()"'}' \
       "$DISCORD_WEBHOOK_URL"
   fi
 }
